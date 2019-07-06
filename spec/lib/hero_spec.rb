a966eef4c04a0dbc807f1f29251839a5694303b0
Diego <diego_shakan@hotmail.com>
2019-07-06 18:49:16 -0300

    challenge of the warrior... Testing HP

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -6,4 +6,8 @@ describe Hero do
 			hero = Hero.new
 			expect(hero.weapon).to  eq('sword')
 		end
+			hero = Hero.new
+		it 'has more than 1000 hp points' do
+			expect(hero.hp).to be > 1000
+		end		
 end
